ae9f3d363e08df98af00f2f5696febf871e43624
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2019-11-19 10:04:12 -0500

    Extract functionality to CGImageOverlayWindow and CGDirectDisplayID.init(screen:)

--- Nightfall/FadeOverlay.swift
+++ Nightfall/FadeOverlay.swift
@@ -1,8 +1,7 @@
 //
-//  TransitionWindow.swift
+//  FadeOverlay.swift
 //  Nightfall
 //
-//  Created by Ryan Thomson on 7/12/19.
 //  Copyright © 2019 Ryan Thomson. All rights reserved.
 //
 
@@ -14,33 +13,19 @@ func showFadeOverlay() {
 	let fadeDuration = defaults.double(forKey: "FadeDuration")
 	
 	for screen in NSScreen.screens {
+		let cgDisplayID = CGDirectDisplayID(screen: screen)
+		guard let screenshot = CGDisplayCreateImage(cgDisplayID) else { continue }
 		
-		// NSScreen and CGDirectDisplayID use different ID systems, and we need both
-		guard let cgDisplayID = screen.deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")]
-			as? CGDirectDisplayID else { continue }
+		let overlay = CGImageOverlayWindow(screenshot, position: screen.frame)
+		overlay.orderFront(nil)
 		
-		let screenshot = CGDisplayCreateImage(cgDisplayID)
-		
-		// Create a window to display as an overlay
-		let overlay = NSWindow(contentRect: screen.frame, styleMask: .borderless, backing: .buffered, defer: true)
-		overlay.level = .statusBar // Place above other windows, dock, and menu bar
-		overlay.collectionBehavior = .stationary // Don't move in mission control and expose
-		overlay.ignoresMouseEvents = true
-		
-		// Display the screenshot on the overlay
-		overlay.contentView?.wantsLayer = true
-		overlay.contentView?.layer?.contents = screenshot
-		
-		overlay.makeKeyAndOrderFront(nil)
-		
-		DispatchQueue.main.asyncAfter(deadline: .now() + fadeDelay) {
-			NSAnimationContext.runAnimationGroup({ (context) in
+		DispatchQueue.main.asyncAfter(deadline: .now() + fadeDelay) { [overlay] in
+			NSAnimationContext.runAnimationGroup({ context in
 				context.duration = fadeDuration
-				overlay.animator().alphaValue = 0
+				overlay.animator().alphaValue = 0.0
 			}, completionHandler: {
 				overlay.orderOut(nil)
 			})
 		}
 	}
-	
 }
