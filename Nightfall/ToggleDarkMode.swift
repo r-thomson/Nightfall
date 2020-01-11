b158c6b2fe7c4437e54e2516ca9e89f1c1ed81bf
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-01-10 23:27:27 -0500

    Add check for System Events permissions to toggleDarkMode()

--- Nightfall/ToggleDarkMode.swift
+++ Nightfall/ToggleDarkMode.swift
@@ -12,6 +12,23 @@ import Cocoa
 /// This function includes behavior not in `setSystemAppearance(to:)`, such as displaying the fade
 /// animation and displaying errors in alerts.
 func toggleDarkMode() {
+	if PermissionUtil.systemEventsPermission(canPrompt: true) != .permitted {
+		let alert = NSAlert()
+		alert.messageText = "System Events are not enabled for Nightfall."
+		alert.informativeText = "Nightfall needs access to System Events to enable and disable dark mode. Enable \"Automation\" for Nightfall in System Preferences to use Nightfall."
+		alert.addButton(withTitle: "OK")
+		alert.addButton(withTitle: "Open System Preferences")
+		
+		if alert.runModal() == .alertSecondButtonReturn {
+			// Opens the Automation section in System Preferences
+			if let url = URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_Automation") {
+				NSWorkspace.shared.open(url)
+			}
+		}
+		
+		return
+	}
+	
 	let defaults = UserDefaults.standard
 	
 	if defaults.useFade {
