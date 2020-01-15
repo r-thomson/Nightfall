30e519ffae0ebc121544d21d9fa8c6b487d4f212
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-01-15 13:02:43 -0500

    Re-enable the fade transition by default

--- Nightfall/AppDelegate.swift
+++ Nightfall/AppDelegate.swift
@@ -18,13 +18,10 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Register user defaults
 		UserDefaults.standard.register(defaults: [
-			UserDefaults.Keys.useFade: false,
+			UserDefaults.Keys.useFade: true,
 			UserDefaults.Keys.fadeDelay: 0.6,
 			UserDefaults.Keys.fadeDuration: 0.6
 		])
-		
-		// TODO: Re-enable fade effect by default
-		// It has been disabled to make setting permissions easier
 		
 		// Register the services provider
 		NSApp.servicesProvider = ServicesProvider()
