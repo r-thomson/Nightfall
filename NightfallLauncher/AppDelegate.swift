136bc443f12526c311fa1d5618ddc60702356301
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-02-06 17:09:12 -0500

    Create launcher helper application

---
+++ NightfallLauncher/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NightfallLauncher
+//
+//  Copyright © 2020 Ryan Thomson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+}
