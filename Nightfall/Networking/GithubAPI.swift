bca62fec3b71e0f4b0203b06d5802fb329b95be8
Ryan Thomson <29545379+r-thomson@users.noreply.github.com>
2020-02-11 20:29:08 -0500

    Implement GitHub API abstraction layer with getLatestRelease()

---
+++ Nightfall/Networking/GithubAPI.swift
@@ -0,0 +1,41 @@
+//
+//  GithubAPI.swift
+//  Nightfall
+//
+//  Copyright © 2020 Ryan Thomson. All rights reserved.
+//
+
+import Foundation
+
+enum GithubAPI {
+	/// The full name of Nightfall's GitHub repository (account name and repo name)
+	static let repoFullName = "r-thomson/Nightfall"
+	
+	/// Base API endpoint for Nightfall's GitHub repository
+	static let repoApiUrl = URL(string: "https://api.github.com/repos/\(repoFullName)/")!
+	
+	/// Fetches the latest non-prerelease, non-draft release from the Nightfall GitHub repository.
+	/// - Parameter completion: Completion handler to be called once the network request is finished.
+	static func getLatestRelease(completion: @escaping (Result<Release, Error>) -> Void) {
+		let endpoint = repoApiUrl.appendingPathComponent("releases/latest")
+		let request = URLRequest(url: endpoint, cachePolicy: .reloadIgnoringLocalCacheData)
+		
+		URLSession.shared.dataTask(with: request) { (data, res, err) in
+			if let error = err {
+				completion(.failure(error))
+				return
+			}
+			
+			// TODO: Handle and call completion() when data is nil
+			if let data = data {
+				do {
+					let decoder = JSONDecoder()
+					let decoded = try decoder.decode(Release.self, from: data)
+					completion(.success(decoded))
+				} catch {
+					completion(.failure(error))
+				}
+			}
+		}.resume()
+	}
+}
---
+++ Nightfall/Networking/Models/GithubAPIModels.swift
@@ -0,0 +1,26 @@
+//
+//  GithubAPIModels.swift
+//  Nightfall
+//
+//  Copyright © 2020 Ryan Thomson. All rights reserved.
+//
+
+import Foundation
+
+extension GithubAPI {
+	/// # Reference
+	/// <https://developer.github.com/v3/repos/releases/>
+	struct Release: Codable {
+		let name: String
+		let version: String
+		let url: URL
+		let isPrerelease: Bool
+		
+		enum CodingKeys: String, CodingKey {
+			case name
+			case version = "tag_name"
+			case url = "html_url"
+			case isPrerelease = "prerelease"
+		}
+	}
+}
