b34bb9c61609b35daae9f7d9cdde305a0aecf68b
Ryan Thomson <git@ryanthomson.net>
2022-01-29 20:01:32 -0500

    Make PreferencesPopover clear its view on close
    This will allow PreferencesView to determine when it's been opened
    and closed.

--- Nightfall/PreferencesPopover.swift
+++ Nightfall/PreferencesPopover.swift
@@ -1,14 +1,15 @@
 import Cocoa
 import SwiftUI
 
-final class PreferencesPopover: NSPopover {
+final class PreferencesPopover: NSPopover, NSPopoverDelegate {
 	static let shared = PreferencesPopover()
+	
+	let view = PreferencesView()
 	
 	override init() {
 		super.init()
 		
 		self.behavior = .transient
-		self.contentViewController = NSHostingController(rootView: PreferencesView())
 	}
 	
 	required init?(coder: NSCoder) {
@@ -17,5 +18,17 @@ final class PreferencesPopover: NSPopover {
 	
 	func show(statusButton: NSStatusBarButton) {
 		show(relativeTo: statusButton.bounds, of: statusButton, preferredEdge: .minY)
+	}
+	
+	override func show(relativeTo positioningRect: NSRect, of positioningView: NSView, preferredEdge: NSRectEdge) {
+		// I thought this would go in popoverWillShow, but that causes a runtime exception
+		// It seems that popoverWillShow is called after show() checks the contentViewController
+		contentViewController = contentViewController ?? NSHostingController(rootView: view)
+
+		super.show(relativeTo: positioningRect, of: positioningView, preferredEdge: preferredEdge)
+	}
+		
+	func popoverDidClose(_ notification: Notification) {
+		contentViewController = nil
 	}
 }
